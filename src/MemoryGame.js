073bbca5d8295d4a05c9bb22f1afd7274fa8dfd0
BoardOnly <dr.yorik@gmail.com>
2018-12-03 22:01:07 +0200

    fixed method flipcard

--- src/MemoryGame.js
+++ src/MemoryGame.js
@@ -13,7 +13,21 @@ class MemoryGame {
     this.lockBoard = false;
     this.countTries = 0;
     this.countPairs = 0;
-  }
+    const _that = this;
+    this.flipCard = function () {
+      if (_that.lockBoard) return;
+      if (this === _that.firstCard) return;
+      this.classList.add('flip');
+            
+      if (_that.hasFlippedCard == false) {
+        _that.hasFlippedCard = true;
+        _that.firstCard = this;
+        return;
+      }
+      _that.secondCard = this;
+      _that.matchCard();
+    }
+}
 
 mixSuits(suits) {
   return suits.sort(() => { return .5 - Math.random() });
@@ -24,7 +38,7 @@ createCards() {
     let img_front = document.createElement('img');
     let img_back = document.createElement('img');
     card.className = 'card';
-    card.addEventListener('click', this.flipCard)
+    card.addEventListener('click', this.flipCard);
     card.id = (`${i}`);
     card.name = `${newSuits}`;
     memory.prepend(card);
@@ -36,20 +50,23 @@ createCards() {
   })
 }
 
-flipCard() {
-
+flipCard () {
   if (this.lockBoard) return;
   if (this === this.firstCard) return;
   this.classList.add('flip');
-
+  console.log(this);
+  
   if (this.hasFlippedCard == false) {
     this.hasFlippedCard = true;
     this.firstCard = this;
+    console.log(this.firstCard);
     return;
   }
   this.secondCard = this;
-  this.matchCard();
+  console.log(this.secondCard, 2);
+  () => matchCard();
 }
+
 
 unflipCard() {
   this.lockBoard = true;
@@ -61,6 +78,7 @@ unflipCard() {
 }
 
 matchCard() {
+  console.log('1');
   this.countTries += 1;
   if (this.firstCard.name === this.secondCard.name) {
     this.disableCards()
@@ -86,22 +104,24 @@ reset() {
 
 win() {
   if (this.countPairs === 8) {
-    this.winText.innerHTML = `Ура. Это Победа за ${this.countTries} ${this.ends(countTries)}!!!`;
+    this.winText.innerHTML = `Ура. Это Победа за ${this.countTries} ${this.ends()}!!!`;
   }
 }
-ends(countTries) {
-  let count = countTries % 100;
+ends() {
+  let count = this.countTries % 100;
   if (count >= 5 && count <= 20) {
-    txt = 'ходов';
+    this.txt = 'ходов';
   } else {
     count = count % 10;
     if (count == 1) {
-      txt = 'ход';
+      this.txt = 'ход';
     } else if (count >= 2 && count <= 4) {
-      txt = 'хода';
+      this.txt = 'хода';
     }
   }
-  return txt;
+  return this.txt;
 }
 }
 
+let q = new MemoryGame();
+q.createCards();
